143d19631663721536a3a7b54e3298bbd6bb8e43
alexcc4 <liangbinsi@gmail.com>
2017-06-15 20:57:51 +0800

    add docker support.

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.5
+MAINTAINER Alex <liangbinsi@gmail.com>
+
+# App path /code
+ENV INSTALL_PATH=/code
+RUN mkdir -p $INSTALL_PATH
+WORKDIR $INSTALL_PATH
+
+# install requirements
+ADD requirements.txt $INSTALL_PATH
+RUN pip install -r requirements.txt
+
+ADD . $INSTALL_PATH
---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -u
+
+cd $(pwd)
+
+# image name
+N=app_api
+
+L=.env.local.latest
+T=.env.local.$(date +%Y%m%d%H%M%S)
+
+grep = .env.${PYTHON_ENV} | awk '{print "export " $0}'| tee ${L} > ${T}
+source ${T}
+
+V=$(date +%F)
+
+# build image api from Dockerfile
+docker build -t "${N}:${V}" -t "${N}:latest" .
+
+
+docker-compose up -d
+
+docker-compose exec -T api python3 manage.py db migrate
+docker-compose exec -T api python3 manage.py db upgrade
